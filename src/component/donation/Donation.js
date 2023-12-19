5d92ec3bb8202b9ba64f9450360eb878c0f21bff
Aditya Verma <119095017+AdityaVerma2003@users.noreply.github.com>
2023-12-19 16:42:31 +0530

    Update Donation.js

--- src/component/donation/Donation.js
+++ src/component/donation/Donation.js
@@ -9,19 +9,19 @@ const Donation = () => {
       <div className="container Dcontact-container">
         <div class="Dright">
       <h5>Donation</h5>
-        <form action='https://docs.google.com/forms/d/e/1FAIpQLSe35i_3zqeXqPEhQoiMmf2-9iRgitGldfrHCxJfXZCkRZOAAg/formResponse' >
-          <input type="text"  class ="Dfield" name="entry.638816101" placeholder="Full Name" required />
-          <input type="number" class ="Dfield" name="entry.1146349216" placeholder="Phone Number"  />
+        <form action='https://docs.google.com/forms/d/e/1FAIpQLSf0dfbG0p-vxq_N8027dUrIVz9mTbs_j1gH6T_uQ9X7yRRUbA/formResponse' >
+          <input type="text"  class ="Dfield" name="entry.144018705" placeholder="Full Name" required />
+          <input type="number" class ="Dfield" name="entry.2054882568" placeholder="Phone Number"  />
 
-          <input type="email" class = "Dfield" name="entry.765868520" placeholder='Email'  />
+          <input type="email" class = "Dfield" name="entry.1708506464" placeholder='Email'  />
 
-          <input list="Donate" placeholder='What you want to Donate?' required name="entry.721389076" />
+          <input list="Donate" placeholder='What you want to Donate?' required name="entry.151041774" />
           <datalist id="Donate">
             <option value="Clothes" />
             <option value="Books" />
             <option value="Food" />
           </datalist>
-          <input list="Donate" placeholder='Donate More?' name="entry.1697845859" />
+          <input list="Donate" placeholder='Donate More?' name="entry.2133965434" />
           <datalist id="Donate">
             <option value="Clothes" />
             <option value="Books" />
